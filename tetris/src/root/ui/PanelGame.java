f5702ec6256f4ac7198c3411386622e149393b9a
tuwq <aheadqiang@126.com>
2018-12-06 13:12:51 +0800

    窗口布局

--- tetris/src/root/ui/PanelGame.java
+++ tetris/src/root/ui/PanelGame.java
@@ -8,12 +8,28 @@ import root.ui.model.Lay;
 
 public class PanelGame extends JPanel {
 	
-	private Lay lay1 = new Lay(128, 32, 320, 576);
+	private Lay lay1;
+	private Lay lay2;
+	private Lay lay3;
+	private Lay[] lays = null;
+	public PanelGame() {
+		lays = new Lay[] {
+				new Lay(40, 32, 334, 279),
+				new Lay(40, 343, 334, 279),
+				new Lay(414, 32, 334, 590),
+				new Lay(788, 32, 334, 124),
+				new Lay(788, 188, 176, 148),
+				new Lay(964, 188, 158, 148),
+				new Lay(788, 368, 334, 200)
+		};
+	}
 	/**
-	 * 绘制单个框
+	 * 绘制框
 	 */
 	@Override
 	protected void paintComponent(Graphics g) {
-		lay1.createWindow(g);
+		for (Lay lay : lays) {
+			lay.createWindow(g);
+		}
 	}
 }
